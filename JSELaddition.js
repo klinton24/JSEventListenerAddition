4bb90ff4c7dbaa8b597c0309c0bc8ba3c640802a
K Smith <klinton.jp@gmail.com>
2018-03-18 18:38:32 -0400

    Coded addition function and changed CSS.

--- JSELaddition.js
+++ JSELaddition.js
@@ -1,2 +1,16 @@
-var numberOne = document.getElementById("numberone");
-var numberTwo = document.getElementById("numbertwo");
+//variables
+var numberOne = document.getElementById("numberOne");
+var numberTwo = document.getElementById("numberTwo");
+var sum = document.getElementById("sum");
+
+numberOne.addEventListener("input", addNumbers);
+numberTwo.addEventListener("input", addNumbers);
+
+
+function addNumbers() {
+    //parse float changes it to a number
+    // Or zero prevents NaN error
+    var first = parseFloat(numberOne.value) || 0;
+    var second = parseFloat(numberTwo.value) || 0;
+    sum.innerHTML = "your sum is: " + (first + second);
+}
